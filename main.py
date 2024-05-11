49a4ed892d430a2d590872913108703cd0158edf
Cristian <cristianguevara0331@gmail.com>
2024-05-11 15:45:29 -0400

    Agregue las funciones para sistema Ternario y Cuaternario

--- main.py
+++ main.py
@@ -20,19 +20,38 @@ def octdec(n):
 
 def dechex(n):
     if n == 0:
-        return '0'
+        return "0"
     else:
         return hex(n).replace("0x", "")
 
 def hexdec(n):
     return int(n, 16)
 
+def decter(n):
+    if n == 0:
+        return "0"
+    else:
+        tern = ""
+        while n > 0:
+            tern = str(n % 3) + tern
+            n = n // 3
+        return tern
 
+def terdec(n):
+    return int(n, 3)
 
+def deccua(n):
+    if n == 0:
+        return "0"
+    else:
+        cuate = ""
+        while n > 0:
+            cuate = str(n % 4) + cuate
+            n = n // 4
+        return cuate
 
-
-
-
+def cuadec(n):
+    return int(n, 4)
 
     
 def main(page: ft.Page):
@@ -62,6 +81,23 @@ def main(page: ft.Page):
             new_task2.value = str(hexdec(new_task.value))
             new_task.focus()
             new_task2.update()
+        elif dd.value=="Decimal a Ternario":
+            new_task2.value = decter(int(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        elif dd.value=="Ternario a Decimal":
+            new_task2.value = str(terdec(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        elif dd.value=="Decimal a Cuaternario":
+            new_task2.value = deccua(int(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        elif dd.value=="Cuaternario a Decimal":
+            new_task2.value = str(cuadec(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        
     def aleatorio_clicked(e):
         if dd.value=="Decimal a Binario":
             new_task.value=rd.randint(0, 1000000)
@@ -85,17 +121,27 @@ def main(page: ft.Page):
         elif dd.value=="Hexadecimal a Decimal":
             new_task.value= dechex(rd.randint(0, 1000000))
             new_task.update()
-            
-        
-        
-  
+        elif dd.value=="Decimal a Ternario":
+            new_task.value=rd.randint(0, 1000000)
+            new_task.update()
+           
+        elif dd.value=="Ternario a Decimal":
+            new_task.value= decter(rd.randint(0, 1000000))
+            new_task.update()
+        elif dd.value=="Decimal a Cuaternario":
+            new_task.value=rd.randint(0, 1000000)
+            new_task.update()
+           
+        elif dd.value=="Cuaternario a Decimal":
+            new_task.value= deccua(rd.randint(0, 1000000))
+            new_task.update()
+               
     def elegirop(e):
         new_task.value=""
         new_task2.value=""
         new_task.focus()
         new_task2.update()
         new_task.update()
-
 
     new_task = ft.TextField(hint_text="Ingrese el digito", width=300)
     new_task2 = ft.TextField(hint_text="", width=300)
@@ -110,6 +156,10 @@ def main(page: ft.Page):
             ft.dropdown.Option("Octal a Decimal",on_click=elegirop),
             ft.dropdown.Option("Decimal a Hexadecimal",on_click=elegirop),
             ft.dropdown.Option("Hexadecimal a Decimal",on_click=elegirop),
+            ft.dropdown.Option("Decimal a Ternario",on_click=elegirop),
+            ft.dropdown.Option("Ternario a Decimal",on_click=elegirop),
+            ft.dropdown.Option("Decimal a Cuaternario",on_click=elegirop),
+            ft.dropdown.Option("Cuaternario a Decimal",on_click=elegirop),
 
         ],
     )
