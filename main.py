a63a9c1f37399d0c09ee3a5c8eb7f88ec98ae0c2
Cristian <cristianguevara0331@gmail.com>
2024-05-11 16:44:19 -0400

    Agregue todas las posibles conversiones y combinaciones que faltaban

--- main.py
+++ main.py
@@ -52,6 +52,50 @@ def deccua(n):
 
 def cuadec(n):
     return int(n, 4)
+def binoct(n):
+    return decoct(bindec(n))
+def octbin(n):
+    return decbin(octdec(n))
+def binhex(n):
+    return dechex(bindec(n))
+def hexbin(n):
+    return decbin(hexdec(n))
+def binter(n):
+    return decter(bindec(n))
+def terbin(n):
+    return decbin(terdec(n))
+def bincua(n):
+    return deccua(bindec(n))
+def cuabin(n):
+    return decbin(cuadec(n))
+def octhex(n):
+    return dechex(octdec(n))
+def hexoct(n):
+    return decoct(hexdec(n))
+def octter(n):
+    return decter(octdec(n))
+def teroct(n):
+    return decoct(terdec(n))
+def octcua(n):
+    return deccua(octdec(n))
+def cuaoct(n):
+    return decoct(cuadec(n))
+def hexter(n):
+    return decter(hexdec(n))
+def terhex(n):
+    return dechex(terdec(n))
+def cuater(n):
+    return decter(cuadec(n))
+def tercua(n):
+    return deccua(terdec(n))
+def hexcua(n):
+    return deccua(hexdec(n))
+def cuahex(n):
+    return dechex(cuadec(n))
+
+
+
+
 
     
 def main(page: ft.Page):
@@ -97,6 +141,95 @@ def main(page: ft.Page):
             new_task2.value = str(cuadec(new_task.value))
             new_task.focus()
             new_task2.update()
+        
+        elif dd.value=="Binario a Octal":
+            new_task2.value = binoct(str(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        elif dd.value=="Octal a Binario":
+            new_task2.value = str(octbin(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        elif dd.value=="Binario a Hexadecimal":
+            new_task2.value = binhex(str(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        elif dd.value=="Hexadecimal a Binario":
+            new_task2.value = str(hexbin(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        elif dd.value=="Binario a Ternario":
+            new_task2.value = binter(str(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        elif dd.value=="Ternario a Binario":
+            new_task2.value = str(terbin(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        elif dd.value=="Binario a Cuaternario":
+            new_task2.value = bincua(str(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        elif dd.value=="Cuaternario a Binario":
+            new_task2.value = str(cuabin(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        elif dd.value=="Octal a Hexadecimal":
+            new_task2.value = octhex(str(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        elif dd.value=="Hexadecimal a Octal":
+            new_task2.value = str(hexoct(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        elif dd.value=="Octal a Ternario":
+            new_task2.value = octter(str(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        elif dd.value=="Ternario a Octal":
+            new_task2.value = str(teroct(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        elif dd.value=="Octal a Cuaternario":
+            new_task2.value = octcua(str(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        elif dd.value=="Cuaternario a Octal":
+            new_task2.value = str(cuaoct(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        elif dd.value=="Hexadecimal a Ternario":
+            new_task2.value = hexter(str(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        elif dd.value=="Ternario a Hexadecimal":
+            new_task2.value = str(terhex(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        elif dd.value=="Cuaternario a Ternario":
+            new_task2.value = cuater(str(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        elif dd.value=="Ternario a Cuaternario":
+            new_task2.value = str(tercua(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        elif dd.value=="Hexadecimal a Cuaternario":
+            new_task2.value = hexcua(str(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        elif dd.value=="Cuaternario a Hexadecimal":
+            new_task2.value = str(cuahex(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        
+        
+
+
+
+
+
+
         
     def aleatorio_clicked(e):
         if dd.value=="Decimal a Binario":
@@ -160,6 +293,33 @@ def main(page: ft.Page):
             ft.dropdown.Option("Ternario a Decimal",on_click=elegirop),
             ft.dropdown.Option("Decimal a Cuaternario",on_click=elegirop),
             ft.dropdown.Option("Cuaternario a Decimal",on_click=elegirop),
+
+            ft.dropdown.Option("Binario a Octal",on_click=elegirop),
+            ft.dropdown.Option("Octal a Binario",on_click=elegirop),
+            ft.dropdown.Option("Binario a Hexadecimal",on_click=elegirop),
+            ft.dropdown.Option("Hexadecimal a Binario",on_click=elegirop),
+            ft.dropdown.Option("Binario a Ternario",on_click=elegirop),
+            ft.dropdown.Option("Ternario a Binario",on_click=elegirop),
+            ft.dropdown.Option("Binario a Cuaternario",on_click=elegirop),
+            ft.dropdown.Option("Cuaternario a Binario",on_click=elegirop),
+            ft.dropdown.Option("Octal a Hexadecimal",on_click=elegirop),
+            ft.dropdown.Option("Hexadecimal a Octal",on_click=elegirop),
+            ft.dropdown.Option("Octal a Ternario",on_click=elegirop),
+            ft.dropdown.Option("Ternario a Octal",on_click=elegirop),
+            ft.dropdown.Option("Octal a Cuaternario",on_click=elegirop),
+            ft.dropdown.Option("Cuaternario a Octal",on_click=elegirop),
+            ft.dropdown.Option("Hexadecimal a Ternario",on_click=elegirop),
+            ft.dropdown.Option("Ternario a Hexadecimal",on_click=elegirop),
+            ft.dropdown.Option("Cuaternario a Ternario",on_click=elegirop),
+            ft.dropdown.Option("Ternario a Cuaternario",on_click=elegirop),
+            ft.dropdown.Option("Hexadecimal a Cuaternario",on_click=elegirop),
+            ft.dropdown.Option("Cuaternario a Hexadecimal",on_click=elegirop),
+
+
+
+
+
+
 
         ],
     )
