2f816ad1663584ef28fa2b15b392a8d3c299c8ce
Cristian <cristianguevara0331@gmail.com>
2024-05-11 15:23:55 -0400

    Agregue las funciones para hexadecimal y ademas agregue un boton para obtener un numero random dependiendo de la opcion de conversion que se escoga en la interfaz

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import flet as ft
+import random as rd
 
 def decbin(n):
     if n == 0:
@@ -16,6 +17,23 @@ def decoct(n):
     
 def octdec(n):
     return int(n, 8)
+
+def dechex(n):
+    if n == 0:
+        return '0'
+    else:
+        return hex(n).replace("0x", "")
+
+def hexdec(n):
+    return int(n, 16)
+
+
+
+
+
+
+
+
     
 def main(page: ft.Page):
     page.add(ft.SafeArea(ft.Text("Traductor de sistemas numericos")))
@@ -36,6 +54,39 @@ def main(page: ft.Page):
             new_task2.value = str(octdec(new_task.value))
             new_task.focus()
             new_task2.update()
+        elif dd.value=="Decimal a Hexadecimal":
+            new_task2.value = dechex(int(new_task.value))
+            new_task.focus()
+            new_task2.update()
+        elif dd.value=="Hexadecimal a Decimal":
+            new_task2.value = str(hexdec(new_task.value))
+            new_task.focus()
+            new_task2.update()
+    def aleatorio_clicked(e):
+        if dd.value=="Decimal a Binario":
+            new_task.value=rd.randint(0, 1000000)
+            new_task.update()
+        elif dd.value=="Binario a Decimal":
+            new_task.value= decbin(rd.randint(0, 1000000))
+            new_task.update()
+          
+        elif dd.value=="Decimal a Octal":
+            new_task.value=rd.randint(0, 1000000)
+            new_task.update()
+           
+        elif dd.value=="Octal a Decimal":
+            new_task.value= decoct(rd.randint(0, 1000000))
+            new_task.update()
+          
+        elif dd.value=="Decimal a Hexadecimal":
+            new_task.value=rd.randint(0, 1000000)
+            new_task.update()
+           
+        elif dd.value=="Hexadecimal a Decimal":
+            new_task.value= dechex(rd.randint(0, 1000000))
+            new_task.update()
+            
+        
         
   
     def elegirop(e):
@@ -48,7 +99,7 @@ def main(page: ft.Page):
 
     new_task = ft.TextField(hint_text="Ingrese el digito", width=300)
     new_task2 = ft.TextField(hint_text="", width=300)
-    page.add(ft.Row([new_task,new_task2, ft.ElevatedButton("Convertir", on_click=convertir_clicked)]))
+    page.add(ft.Row([new_task,new_task2, ft.ElevatedButton("Convertir", on_click=convertir_clicked), ft.ElevatedButton("Aleatorio", on_click=aleatorio_clicked)]))
    
     dd = ft.Dropdown(
         width=300,
@@ -57,6 +108,8 @@ def main(page: ft.Page):
             ft.dropdown.Option("Binario a Decimal", on_click=elegirop),
             ft.dropdown.Option("Decimal a Octal",on_click=elegirop),
             ft.dropdown.Option("Octal a Decimal",on_click=elegirop),
+            ft.dropdown.Option("Decimal a Hexadecimal",on_click=elegirop),
+            ft.dropdown.Option("Hexadecimal a Decimal",on_click=elegirop),
 
         ],
     )
